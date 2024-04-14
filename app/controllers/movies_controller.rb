d744c277257822a403574e764a8e519726fe88d5
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-14 19:12:49 +0000

    finished updateing movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,17 @@ class MoviesController < ApplicationController
     render({ :template => "movie_templates/index" })
   end
 
+  def create
+    the_movie = Movie.new
+    the_movie.title = params.fetch("title_input")
+    the_movie.year = params.fetch("year_input")
+    the_movie.duration = params.fetch("duration_input")
+    the_movie.description = params.fetch("description_input")
+    the_movie.image = params.fetch("image_input")
+    the_movie.save
+    redirect_to("/movies")
+  end
+
   def show
     the_id = params.fetch("path_id")
 
@@ -13,5 +24,24 @@ class MoviesController < ApplicationController
     @the_movie = matching_movies.at(0)
 
     render({ :template => "movie_templates/show" })
+  end
+
+  def delete_this
+    the_id = params.fetch("path_id")
+    the_movie = Movie.find(the_id)
+    the_movie.destroy
+    redirect_to("/movies")
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_movie = Movie.find(the_id)
+    the_movie.title = params.fetch("title_input")
+    the_movie.year = params.fetch("year_input")
+    the_movie.duration = params.fetch("duration_input")
+    the_movie.description = params.fetch("description_input")
+    the_movie.image = params.fetch("image_input")
+    the_movie.save
+    redirect_to("/movies/#{the_movie.id}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
   post("directors", { :controller => "directors", :action => "create" })
-  get("directors/delete_director/:path_id", { :controller => "directors", :action => "delete_this" })
+  get("/delete_director/:path_id", { :controller => "directors", :action => "delete_this" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post("/directors/:path_id", { :controller => "directors", :action => "update" })
 
   get("/movies", { :controller => "movies", :action => "index" })
+  post("movies", { :controller => "movies", :action => "create" })
+  post("delete_movie/:path_id", { :controller => "movies", :action => "delete_this" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  post("/movies/:path_id", { :controller => "movies", :action => "update" })
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
